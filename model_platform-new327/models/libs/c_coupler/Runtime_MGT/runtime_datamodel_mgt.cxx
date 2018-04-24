29100ad172126f10d2a0f7aa68d3a6e9f9082a2a
Sherry <2892285844@qq.com>
2018-04-24 09:40:00 +0800

    runtime_datamodel_mgt.cxx

---
+++ model_platform-new327/models/libs/c_coupler/Runtime_MGT/runtime_datamodel_mgt.cxx
@@ -0,0 +1,14 @@
+/***************************************************************
+  *  Copyright (c) 2017, Tsinghua University.
+  *  This is a source file of C-Coupler.
+  *  This file was initially finished by Dr. Li Liu. 
+  *  If you have any problem, 
+  *  please contact Dr. Li Liu via liuli-cess@tsinghua.edu.cn
+  ***************************************************************/
+
+
+#include <dirent.h>
+#include <stdlib.h>
+#include "runtime_datamodel_mgt.h"
+#include "memory_mgt.h"
+#include "field_info_mgt.h"
